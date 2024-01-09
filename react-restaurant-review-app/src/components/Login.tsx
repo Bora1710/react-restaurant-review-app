4d5e0d31e0a9aacacae4b45835516ee2470657b6
Bora1710 <milosevicb92@gmail.com>
2024-01-09 18:07:47 +0100

    Create login form

--- react-restaurant-review-app/src/components/Login.tsx
+++ react-restaurant-review-app/src/components/Login.tsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [formData, setFormData] = useState({ userName: "", password: "" });
+
+  function handleFormChange(event: any) {
+    const { name, value } = event.target;
+    setFormData((prevValue) => {
+      return { ...prevValue, [name]: value };
+    });
+  }
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    console.log("Username:", formData.userName);
+    console.log("Password:", formData.password);
+  }
   return (
     <>
-      <h1>Login works</h1>
-      <h2>GG</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="userName">Username:</label>
+        <input
+          id="userName"
+          type="text"
+          value={formData.userName}
+          name="userName"
+          onChange={handleFormChange}
+        ></input>
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          type="password"
+          value={formData.password}
+          onChange={handleFormChange}
+          name="password"
+        ></input>
+        <button>Submit</button>
+      </form>
     </>
   );
 }

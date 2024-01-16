de0fcb408c4916b62699a610342c2764b81b68ea
Bora1710 <milosevicb92@gmail.com>
2024-01-16 17:26:13 +0100

    WIP: create register page form

--- react-restaurant-review-app/src/components/Register/Register.tsx
+++ react-restaurant-review-app/src/components/Register/Register.tsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+import { User } from "../../models/user";
+
 export default function Register() {
-  return <h1>it works!</h1>;
+  const [formData, setFormData] = useState<User>({
+    id: "",
+    userName: "",
+    password: "",
+    role: 0,
+  });
+
+  function handleFormChange() {}
+
+  function handleSubmit() {}
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="userName">Username:</label>
+        <input
+          id="userName"
+          type="text"
+          value={formData.userName}
+          name="userName"
+          onChange={handleFormChange}
+        ></input>
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          type="password"
+          value={formData.password}
+          onChange={handleFormChange}
+          name="password"
+        ></input>
+        <button>Submit</button>;
+      </form>
+    </>
+  );
 }
